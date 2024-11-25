1ca79b15a7b18612b5565106d27771c324747ab7
BrianTDelaney <brian.delaney@ufl.edu>
2024-11-25 01:47:02 -0500

    Made rudimentary comparison function based on publisher info and tags.

--- graph.py
+++ graph.py
@@ -18,10 +18,16 @@ class Graph:
         for game in response.json().values(): # Iterates through data
             traitList = []
             for trait in game.items():
-                if trait[0] != "name" and trait[0] != "appid":
+                if trait[0] != "name":
                     traitList.append(trait[1]) # Makes a list containing all relevant data for edge creation
+                if trait[0] == "appid":
+                    link = "https://steamspy.com/api.php?request=appdetails&appid=" + str(trait[1])
+                    response2 = requests.get(link)
+                    traitList.append(response2.json()["tags"])
 
             self.nameMap[game["name"]] = Node(game["name"], traitList)
+            print(game["name"])
+            print(traitList)
 
     def initializeMatrix(self): # [NOT FINISHED] This function will be used to initialize the edges in the adjacency matrix
         for node in self.nameMap.values():
@@ -30,5 +36,27 @@ class Graph:
 
     def compareNodes(self, nodeOne, nodeTwo): # [NOT FINISHED] This function will compare nodes to see if they are similar enough for an edge
         similarityScore = 0
+        for index in range(2, 4):
+            if nodeOne.traits[index] == nodeTwo.traits[index]:
+                similarityScore += 1
+        priceDiff = abs(int(nodeOne.traits[14]) - int(nodeTwo.traits[14]))/10000.0
+        similarityScore += 1 - priceDiff
+        for tag in nodeOne.traits[1].items():
+            if tag[0] in nodeTwo.traits[1]:
+                similarityScore += 1
+                print(tag[0])
+        # voteTotalOne = 0
+        # for tag in nodeOne.traits[1].values():
+        #     voteTotalOne += tag[0]
+        # voteTotalTwo = 0
+        # for tag in nodeTwo.traits[1].values():
+        #     voteTotalTwo += tag[0]
+        # for tag in nodeOne.traits[1].values():
+        #     if nodeTwo.traits[1][tag[0]] == tag[1]:
+
+        return similarityScore
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from graph import Graph
 
 games = Graph()
+print(games.compareNodes(games.nameMap["The Elder Scrolls V: Skyrim"], games.nameMap["Fallout 4"]))

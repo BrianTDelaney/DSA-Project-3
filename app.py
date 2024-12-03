d9f4d714dc3bc96171601357235c843ea7912407
mattl <matt.lozito@gmail.com>
2024-12-02 23:23:38 -0500

    more pages and changed formatting

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/ranker')
-def ranker():  # put application's code here
-    return render_template('ranker.html')
+@app.route('/home')
+def home():  # put application's code here
+    return render_template('home.html')
 
 @app.route('/recommender')
 def recommender():  # put application's code here
     return render_template('recommender.html')
+
+@app.route('/results')
+def results():  # put application's code here
+    return render_template('results.html')
+
+@app.route('/topgames')
+def topgames():  # put application's code here
+    return render_template('topgames.html')
 
 
 if __name__ == '__main__':

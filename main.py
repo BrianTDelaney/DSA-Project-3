e353a55732172eef85390ae4bbadd556a8abe737
BrianTDelaney <brian.delaney@ufl.edu>
2024-12-04 18:47:55 -0500

    Added note to main.py to avoid confusion

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from graph import Graph
 from ai import generate_prompt
 from heap import Heap
 
+# NOTE: THIS FILE IS FOR TEST PURPOSES ONLY AND IS NOT USED TO RUN THE PROGRAM
+# PLEASE REFER TO APP.PY FOR SUCH PURPOSES
+
 # games = Graph() # THIS CODE WILL TAKE A MINUTE TO RUN
 # input = ["Portal 2", "Half-Life 2", "Counter-Strike: Global Offensive"]
 # print(games.recommend(input))
